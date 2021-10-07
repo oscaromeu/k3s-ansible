6441396bfc4cd1da818c8f018b5c157574746189
oscar.romeu <oscaromeu@gmail.com>
2021-10-07 08:07:12 +0100

    Add podinfo Kustomization

---
+++ infra/test/argocd/test.sh
@@ -0,0 +1,10 @@
+flux bootstrap github \
+  --owner=$GITHUB_USER \
+  --repository=k3s-ansible-deploy-monitoring-playground \
+  --branch=master \
+  --path=./clusters/test \
+  --personal
+ 
+ 
+ 
+ 
